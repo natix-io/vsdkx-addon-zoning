ca8c2c0617b429f501015f4caffec91c2a9b39e9
Guja Mekokishvili <g.mekokishvili@omedia.ge>
2022-04-14 17:42:52 +0400

    perf: unit test post process for one and two bounding boxes

--- tests/test_addon.py
+++ tests/test_addon.py
@@ -14,11 +14,11 @@ class TestAddon(unittest.TestCase):
         addon_config = {
             "remove_areas": [[]],
             "zones": [[]],
-            "class_names": []
+            "class_names": ["Person"]
         }
 
         model_config = {
-            "filter_class_ids": []
+            "filter_class_ids": [0]
         }
 
         TestAddon.ADDON_OBJECT = ZoneProcessor(addon_config, {}, model_config,
@@ -57,3 +57,127 @@ class TestAddon(unittest.TestCase):
         self.assertEqual(
             pre_processed_addon_object.shared, {}
         )
+
+    def test_post_process_one_bbox(self):    
+        frame = np.zeros((640, 480, 3)).astype('uint8')
+        inference = Inference()
+        shared = {}
+
+        addon_object = AddonObject(frame=frame, inference=inference,
+                                   shared=shared)        
+        
+        TestAddon.ADDON_OBJECT._remove_areas = [[]]
+        TestAddon.ADDON_OBJECT._zones = [
+                [[100, 100],
+                 [200, 50],
+                 [400, 100],
+                 [400, 400],
+                 [100, 400],
+                 [100, 100]]
+            ]
+        
+        bb_1 = np.array([120, 150, 170, 200])
+        c_1 = [145, 175]
+        
+        boxes = [bb_1]
+        
+        trackable_object_1 = TrackableObject(0, c_1, bb_1)
+        trackable_object_1.centroids.extend([c_1, c_1])
+
+        shared = {
+            "trackable_objects": {
+                "0": trackable_object_1,
+            }
+        }
+        
+        addon_object.inference.boxes = boxes
+        addon_object.inference.classes = np.array([0])
+        addon_object.shared = shared
+
+        post_processed_addon_object = TestAddon.ADDON_OBJECT.post_process(addon_object)
+        
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["zone_0"]["Person_count"], 
+            1
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["zone_0"]["Person"], 
+            [0]
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["rest"]["Person_count"], 
+            0
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["rest"]["Person"], 
+            []
+        )
+
+    def test_post_process_two_bbox(self):
+        frame = np.zeros((640, 480, 3)).astype('uint8')
+        inference = Inference()
+        shared = {}
+
+        addon_object = AddonObject(frame=frame, inference=inference,
+                                   shared=shared)        
+        
+        TestAddon.ADDON_OBJECT._remove_areas = [[]]
+        TestAddon.ADDON_OBJECT._zones = [
+                [[100, 100],
+                 [200, 50],
+                 [400, 100],
+                 [400, 400],
+                 [100, 400],
+                 [100, 100]]
+            ]
+        
+        bb_1 = np.array([120, 150, 170, 200])
+        c_1 = [145, 175]
+        
+        trackable_object_1 = TrackableObject(0, c_1, bb_1)
+        trackable_object_1.centroids.extend([c_1, c_1])
+       
+        bb_2 = np.array([0, 0, 50 , 45 ])
+        c_2 = [25, 22.5]
+        
+        trackable_object_2 = TrackableObject(1, c_2, bb_2)
+        trackable_object_2.centroids.extend([c_2, c_2])
+ 
+        boxes = [bb_1, bb_2]
+
+        shared = {
+            "trackable_objects": {
+                "0": trackable_object_1,
+                "1": trackable_object_2
+            }
+        }
+
+        addon_object.inference.boxes = boxes
+        addon_object.inference.classes = np.array([0, 0])
+        addon_object.shared = shared
+
+        post_processed_addon_object = TestAddon.ADDON_OBJECT.post_process(addon_object)
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["zone_0"]["Person_count"], 
+            1
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["zone_0"]["Person"], 
+            [0]
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["rest"]["Person_count"], 
+            1
+        )
+
+        self.assertEqual(
+            post_processed_addon_object.inference.extra["zoning"]["rest"]["Person"], 
+            [1]
+        )
+
